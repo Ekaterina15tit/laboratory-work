f470eaf71a5d10fcf3c1aec3ca11d58bff08d032
Ekaterina15tit <76525883+Ekaterina15tit@users.noreply.github.com>
2020-12-25 15:02:25 +0500

    laboratory work
    laboratory work

---
+++ лабораторная 1. float/лабораторная 1. float/лабораторная 1. float.cpp
@@ -0,0 +1,16 @@
+﻿#include <cstdlib>
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    float a, b, c, d, e;
+    cout << " Введите a: " << endl;
+    cin >> a;
+    cout << " Введите b: " << endl;
+    cin >> b;
+    c = pow(a + b, 3); d = pow(a, 3) + 3 * pow(a, 2) * b;
+    e = 3 * a * pow(b, 2) + pow(b, 3);
+    cout << "Ваш результат :" << (float)(c - d) / e << endl;
+}
